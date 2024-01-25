769e6f61cea56f00d246e8c91f06146421521b8c
anirutsolrak <146773453+anirutsolrak@users.noreply.github.com>
2024-01-25 13:31:44 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
 let listaDeNumerosSorteados = [];
 let numeroLimite = 20;
-let numeroSecreto = 7;
+let numeroSecreto = gerarNumeroAleatório ();
 let tentativas = 1;
-let numeroPar = [];
-let numeroImpar = [];
 
-    
-
-    
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector (tag);
     campo.innerHTML = texto;
@@ -19,17 +14,10 @@ exibirTextoNaTela('p', 'Escolha um numero entre 1 e 20')
 };
 exibirMensagemInicial();
 
-    
 function verificarChute() {
     let chute = document.querySelector ('input').value;
-    verificarParOuImpar(chute);
-   
-    if (tentativas ===3) {exibirTextoNaTela ('p' ,'voce atingiu o maximo de tentativas e perdeu')
-    
-}
-  
 
-   else if (chute == numeroSecreto) {
+    if (chute == numeroSecreto) {
        exibirTextoNaTela ('h1', 'Você Acertou!');
        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
        let mensagemTentativas = `Voce descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
@@ -38,26 +26,14 @@ function verificarChute() {
 
         else {
             if (chute > numeroSecreto) {
-exibirTextoNaTela('p','O numero secreto é menor')          
+exibirTextoNaTela('p','O numero secreto é menor');               
             
             }else{
             exibirTextoNaTela('p', 'O numero secreto é maior');
         }
-        
         tentativas++;
         limparCampo();
-        
 }
-      
-
-}
-function verificarParOuImpar(chute ) {
-  
-    if (chute % 2 === 0) {numeroPar.push(chute);
-      
-    } else {numeroImpar.push(chute);
-        
-    }
 }
 function gerarNumeroAleatório() {
 let numeroEscolhido = parseInt(Math.random () * numeroLimite + 1);  
@@ -80,15 +56,8 @@ function limparCampo() {
 };
 function reiniciarJogo() {
     numeroSecreto = gerarNumeroAleatório();
-    limparCampo(); 
+    limparCampo();
     tentativas = 1;
    exibirMensagemInicial();
    document.getElementById('reiniciar').setAttribute('disabled', true);
-   listaFinal();
-};
-function listaFinal() {
-    alert ('numero(s) impar')
-    alert (numeroImpar )
-    alert ('numero(s) par');
-    alert (numeroPar)
-};
+}
